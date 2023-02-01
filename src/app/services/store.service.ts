29f6231bab2ee1eb2ad60f2ba1368b65613c51e9
Jhonanderson Tellez <tellezjhonander@gmail.com>
2023-02-01 13:39:38 -0500

    building subscriptions for the shopping cart

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
-
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,9 +11,12 @@ export class StoreService {
   total = 0;
 
   private myShoppingCart: Product[] = [];
+  private myCart = new BehaviorSubject<Product[]>([]);
+  myCart$ = this.myCart.asObservable();
 
   addProduct(product:Product) {
     this.myShoppingCart.push(product);
+    this.myCart.next(this.myShoppingCart);
   }
   getTotal(){
     return this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0 );
